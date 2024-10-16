4e6d41e179e5365583ba6d623e3106632c57e838
PokiSan <larin44@inbox.ru>
2024-10-16 23:38:24 +0300

    fisrt push to new branch

--- app/src/main/java/com/example/timecalculator/MainActivity.kt
+++ app/src/main/java/com/example/timecalculator/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.timecalculator
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var toolbar: Toolbar
     private lateinit var timeOne: EditText
     private lateinit var timeTwo:EditText
     private lateinit var plusBtn:Button
@@ -23,6 +31,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbar)
+        title = "Калькулятор времени"
+        toolbar.subtitle = "Версия 1.1"
+        toolbar.setLogo(R.drawable.baseline_alarm_24)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -36,16 +50,58 @@ class MainActivity : AppCompatActivity() {
         resultTxt=findViewById(R.id.resultTxt)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.resetMenuBtn->{
+                timeOne.text.clear()
+                timeTwo.text.clear()
+                resultTxt.setTextColor(Color.parseColor("#000000"))
+                resultTxt.text = "Результат"
+                Toast.makeText(
+                    applicationContext,
+                    "Данные очищены",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            R.id.exitMenuBtn -> {
+                Toast.makeText(
+                    applicationContext,
+                    "Приложение закрыто",
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     @SuppressLint("SetTextI18n")
     fun onPlus(view: View) {
+        val timeRegex = Regex("^(?!\\s*\$)(?:[0-9]+[hms])+\$")
         val one = timeOne.text.toString()
         val two = timeTwo.text.toString()
+        if (!timeRegex.matches(one) || !timeRegex.matches(two)){
+            resultTxt.text = "Ошибка ввода!"
+            resultTxt.setTextColor(Color.parseColor("red"))
+            return
+        }
         resultTxt.text = action(one, two, '+')
     }
     @SuppressLint("SetTextI18n")
     fun onMinus(view: View) {
+        val timeRegex = Regex("^(?!\\s*\$)(?:[0-9]+[hms])+\$")
         val one = timeOne.text.toString()
         val two = timeTwo.text.toString()
+        if (!timeRegex.matches(one) || !timeRegex.matches(two)){
+            resultTxt.text = "Ошибка ввода!"
+            resultTxt.setTextColor(Color.parseColor("red"))
+            return
+        }
         resultTxt.text = action(one, two, '-')
     }
 
@@ -60,6 +116,12 @@ class MainActivity : AppCompatActivity() {
         if (h>0) result += "${h}h"
         if (m>0) result += "${m}m"
         if (s>0) result += "${s}s"
+        Toast.makeText(
+            applicationContext,
+            "Результат: $result",
+            Toast.LENGTH_LONG
+        ).show()
+        resultTxt.setTextColor(Color.parseColor("#8B0000"))
         return result
     }
     private fun timeToMap(time:String):Int {
